73c80eb9c5a1e144f8634803bf256eed7ea5e5ef
brice croix <brice.croix@gmail.com>
2023-08-01 00:05:45 +0200

    Added color scheme in App constructor.

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:movna/core/injection.dart';
 import 'package:movna/presentation/router/router.dart';
+import 'package:movna/presentation/themes.dart';
 
 class MovnaApp extends StatelessWidget {
   const MovnaApp({super.key});
@@ -11,6 +12,8 @@ class MovnaApp extends StatelessWidget {
     return MaterialApp.router(
       routerConfig: injector<AppRouter>(),
       debugShowCheckedModeBanner: kDebugMode,
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
     );
   }
 }
