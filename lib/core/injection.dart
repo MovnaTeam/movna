40d7d8ceb93f17e603f2cd0c6ca1184df93326d9
rdahuron <robin.dahuron@cyim.com>
2023-08-01 21:28:29 +0200

    chore(linter): Add linter rules - fix existing lints

--- lib/core/injection.dart
+++ lib/core/injection.dart
@@ -1,6 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-import 'injection.config.dart';
+import 'package:movna/core/injection.config.dart';
 
 final injector = GetIt.instance;
 
