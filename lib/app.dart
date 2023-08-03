f9abf970dafb3d3274bc27ad5758c403de6f778f
rdahuron <robin.dahuron@cyim.com>
2023-08-03 22:43:24 +0200

    refac(locales): PR review

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:movna/core/injection.dart';
+import 'package:movna/jsons.dart';
 import 'package:movna/presentation/locale/locales_helper.dart';
 import 'package:movna/presentation/router/router.dart';
 import 'package:movna/presentation/theme/app_theme.dart';
@@ -17,6 +18,9 @@ class MovnaApp extends StatelessWidget {
       theme: injector<AppTheme>().buildLight(),
       darkTheme: injector<AppTheme>().buildDark(),
       supportedLocales: AppI18n.supportedLocales,
+      onGenerateTitle: (context) {
+        return LocaleKeys.appTitle().translate(context);
+      },
       localizationsDelegates: [
         AppI18n.delegate,
         GlobalMaterialLocalizations.delegate,
