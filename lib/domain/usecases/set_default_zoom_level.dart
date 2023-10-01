2c6541545a2a9e968a6237983bc3c4fd143bb9e4
rdahuron <robin.dahuron@cyim.com>
2023-10-01 19:15:37 +0200

    chore(23-map-zoom): Store zoom level to use it as default on subsequent launches. Prevent zooming and rotating at the same time.

---
+++ lib/domain/usecases/set_default_zoom_level.dart
@@ -0,0 +1,17 @@
+import 'package:injectable/injectable.dart';
+import 'package:movna/domain/faults.dart';
+import 'package:movna/domain/repositories/user_repository.dart';
+import 'package:movna/domain/usecases/base_usecases.dart';
+import 'package:result_dart/result_dart.dart';
+
+@injectable
+class SetDefaultZoomLevel implements UseCaseAsync<Unit, double> {
+  SetDefaultZoomLevel(this._repository);
+
+  final UserRepository _repository;
+
+  @override
+  Future<Result<Unit, Fault>> call(double zoomLevel) {
+    return _repository.setDefaultZoomLevel(zoomLevel);
+  }
+}
