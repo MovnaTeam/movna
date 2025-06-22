d3a7c0f6b6c531137cc3f0790ff68a0b4302c988
bcroix <brice.croix@gmail.com>
2025-06-22 12:08:13 +0200

    Add back map on home (yay !)

--- lib/presentation/screens/home/home_page.dart
+++ lib/presentation/screens/home/home_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movna/assets.dart';
+import 'package:movna/core/injection.dart';
 import 'package:movna/jsons.dart';
+import 'package:movna/presentation/blocs/location_cubit.dart';
+import 'package:movna/presentation/blocs/location_service_cubit.dart';
+import 'package:movna/presentation/blocs/permissions_cubit.dart';
 import 'package:movna/presentation/locale/locales_helper.dart';
+import 'package:movna/presentation/screens/activity/views/map/activity_map_view.dart';
 import 'package:movna/presentation/screens/home/start_activity_popup.dart';
+import 'package:movna/presentation/widgets/none_widget.dart';
 import 'package:movna/presentation/widgets/svg_themed_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -16,16 +23,65 @@ class HomePage extends StatelessWidget {
       ),
       body: SafeArea(
         bottom: false,
-        child: Center(
-          child: ElevatedButton(
-            onPressed: () => showModalBottomSheet<void>(
-              context: context,
-              builder: (context) => const StartActivityPopup(),
+        child: Stack(
+          children: [
+            _buildMapWidget(context),
+            SafeArea(
+              bottom: true,
+              child: Column(
+                children: [
+                  Expanded(child: const NoneWidget()),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () => showModalBottomSheet<void>(
+                        context: context,
+                        builder: (context) => const StartActivityPopup(),
+                      ),
+                      child: Text(
+                        LocaleKeys.home.startActivity().translate(context),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            child: Text(LocaleKeys.home.startActivity().translate(context)),
-          ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildMapWidget(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<LocationServiceCubit>(
+          create: (_) => injector(),
+        ),
+        BlocProvider<PermissionsCubit>(
+          lazy: false,
+          create: (_) {
+            return injector(
+              param1: const PermissionsCubitParams(
+                requestLocation: true,
+                requestNotifications: true,
+              ),
+            )..requestPermissions();
+          },
+        ),
+        BlocProvider<LocationCubit>(
+          lazy: false,
+          create: (providerContext) {
+            return injector(
+              param1: LocationCubitParams(
+                permissionsCubit: providerContext.read<PermissionsCubit>(),
+                locationServiceCubit:
+                    providerContext.read<LocationServiceCubit>(),
+              ),
+            )..listenToLocation();
+          },
+        ),
+      ],
+      child: const ActivityMapView(),
     );
   }
 }
