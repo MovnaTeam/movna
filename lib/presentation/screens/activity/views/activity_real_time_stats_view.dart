4587aac60546060e4891001cebe43ab604f24077
bcroix <brice.croix@gmail.com>
2025-06-24 00:21:28 +0200

    Add statistics

--- lib/presentation/screens/activity/views/activity_real_time_stats_view.dart
+++ lib/presentation/screens/activity/views/activity_real_time_stats_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movna/domain/entities/location.dart';
+import 'package:movna/presentation/blocs/activity_cubit.dart';
 import 'package:movna/presentation/blocs/location_cubit.dart';
+import 'package:movna/presentation/screens/common/widgets/none_widget.dart';
 
 class ActivityRealTimeStatsView extends StatelessWidget {
   const ActivityRealTimeStatsView({super.key});
@@ -9,16 +10,74 @@ class ActivityRealTimeStatsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        BlocSelector<LocationCubit, LocationCubitState, Location?>(
-          selector: (state) => state.location?.location,
-          builder: (BuildContext context, currentLocation) => Center(
-            child: Text(
-              '${currentLocation?.speedInMetersPerSecond.toStringAsFixed(1) ?? '-'} m/s',
-            ),
+        BlocSelector<ActivityCubit, ActivityState, double?>(
+          selector: (state) => state.activity?.distanceInMeters,
+          builder: (BuildContext context, distance) =>
+              ActivityRealTimeStatWidget(
+            title: 'Distance', // TODO translate
+            value: distance,
+            unit: 'm',
+          ),
+        ),
+        BlocSelector<LocationCubit, LocationCubitState, double?>(
+          selector: (state) => state.location?.location.speedInMetersPerSecond,
+          builder: (BuildContext context, speed) => ActivityRealTimeStatWidget(
+            title: 'Speed', // TODO translate
+            value: speed,
+            unit: 'm/s',
+            decimals: 1,
+          ),
+        ),
+        BlocSelector<ActivityCubit, ActivityState, double?>(
+          selector: (state) => state.activity?.averageSpeedInMetersPerSecond,
+          builder: (BuildContext context, speed) => ActivityRealTimeStatWidget(
+            title: 'Average Speed', // TODO translate
+            value: speed,
+            unit: 'm/s',
+            decimals: 1,
           ),
         ),
       ],
+    );
+  }
+}
+
+class ActivityRealTimeStatWidget extends StatelessWidget {
+  const ActivityRealTimeStatWidget({
+    super.key,
+    this.value,
+    this.title,
+    this.unit,
+    this.decimals = 0,
+  });
+
+  final double? value;
+  final String? title;
+  final String? unit;
+  final int decimals;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          title != null ? Center(child: Text(title!)) : NoneWidget(),
+          Center(
+            child: Text(
+              style: DefaultTextStyle.of(context).style.apply(
+                  fontSizeFactor: 2.0,
+                  fontWeightDelta: 5,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+              value?.toStringAsFixed(decimals) ?? '-',
+            ),
+          ),
+          unit != null ? Center(child: Text(unit!)) : NoneWidget(),
+        ],
+      ),
     );
   }
 }
