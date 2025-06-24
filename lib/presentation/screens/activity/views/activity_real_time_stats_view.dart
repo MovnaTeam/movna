5f789d03b85b5ea568189f355d3fde7769d2ba8b
bcroix <brice.croix@gmail.com>
2025-06-25 00:28:31 +0200

    Add activity duration in GUI

--- lib/presentation/screens/activity/views/activity_real_time_stats_view.dart
+++ lib/presentation/screens/activity/views/activity_real_time_stats_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:movna/jsons.dart';
 import 'package:movna/presentation/blocs/activity_cubit.dart';
 import 'package:movna/presentation/blocs/location_cubit.dart';
@@ -11,60 +12,77 @@ class ActivityRealTimeStatsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Column(
       children: [
-        Expanded(
-          child: BlocSelector<ActivityCubit, ActivityState, double?>(
-            selector: (state) => state.activity?.distanceInMeters,
-            builder: (BuildContext context, distance) {
-              var value = distance;
-              var decimals = 0;
-              var unit = LocaleKeys.units.metersShort().translate(context);
-              if (distance != null && distance >= 1000) {
-                value = distance / 1000;
-                decimals = 1;
-                unit = LocaleKeys.units.kilometersShort().translate(context);
-              }
-              return ActivityRealTimeStatWidget(
-                title: LocaleKeys.activity.statistics
-                    .distance()
-                    .translate(context),
-                value: value,
-                unit: unit,
-                decimals: decimals,
-              );
-            },
-          ),
-        ),
-        Expanded(
-          child: BlocSelector<LocationCubit, LocationCubitState, double?>(
-            selector: (state) =>
-                state.location?.location.speedInMetersPerSecond,
-            builder: (BuildContext context, speed) =>
-                ActivityRealTimeStatWidget(
-              title: LocaleKeys.activity.statistics.speed().translate(context),
-              value: speed != null ? speed * 3600 / 1000 : speed,
-              unit:
-                  LocaleKeys.units.kilometersPerHourShort().translate(context),
-              decimals: 1,
+        Center(
+          child: BlocSelector<ActivityCubit, ActivityState, Duration?>(
+            selector: (state) => state.activity?.duration,
+            builder: (BuildContext context, duration) => ActivityDurationWidget(
+              duration: duration,
             ),
           ),
         ),
-        Expanded(
-          child: BlocSelector<ActivityCubit, ActivityState, double?>(
-            selector: (state) => state.activity?.averageSpeedInMetersPerSecond,
-            builder: (BuildContext context, speed) =>
-                ActivityRealTimeStatWidget(
-              title: LocaleKeys.activity.statistics
-                  .averageSpeed()
-                  .translate(context),
-              value: speed != null ? speed * 3600 / 1000 : speed,
-              unit:
-                  LocaleKeys.units.kilometersPerHourShort().translate(context),
-              decimals: 1,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Expanded(
+              child: BlocSelector<ActivityCubit, ActivityState, double?>(
+                selector: (state) => state.activity?.distanceInMeters,
+                builder: (BuildContext context, distance) {
+                  var value = distance;
+                  var decimals = 0;
+                  var unit = LocaleKeys.units.metersShort().translate(context);
+                  if (distance != null && distance >= 1000) {
+                    value = distance / 1000;
+                    decimals = 1;
+                    unit =
+                        LocaleKeys.units.kilometersShort().translate(context);
+                  }
+                  return ActivityRealTimeStatWidget(
+                    title: LocaleKeys.activity.statistics
+                        .distance()
+                        .translate(context),
+                    value: value,
+                    unit: unit,
+                    decimals: decimals,
+                  );
+                },
+              ),
             ),
-          ),
+            Expanded(
+              child: BlocSelector<LocationCubit, LocationCubitState, double?>(
+                selector: (state) =>
+                    state.location?.location.speedInMetersPerSecond,
+                builder: (BuildContext context, speed) =>
+                    ActivityRealTimeStatWidget(
+                  title:
+                      LocaleKeys.activity.statistics.speed().translate(context),
+                  value: speed != null ? speed * 3600 / 1000 : speed,
+                  unit: LocaleKeys.units
+                      .kilometersPerHourShort()
+                      .translate(context),
+                  decimals: 1,
+                ),
+              ),
+            ),
+            Expanded(
+              child: BlocSelector<ActivityCubit, ActivityState, double?>(
+                selector: (state) =>
+                    state.activity?.averageSpeedInMetersPerSecond,
+                builder: (BuildContext context, speed) =>
+                    ActivityRealTimeStatWidget(
+                  title: LocaleKeys.activity.statistics
+                      .averageSpeed()
+                      .translate(context),
+                  value: speed != null ? speed * 3600 / 1000 : speed,
+                  unit: LocaleKeys.units
+                      .kilometersPerHourShort()
+                      .translate(context),
+                  decimals: 1,
+                ),
+              ),
+            ),
+          ],
         ),
       ],
     );
@@ -104,6 +122,43 @@ class ActivityRealTimeStatWidget extends StatelessWidget {
           ),
           unit != null ? Center(child: Text(unit!)) : NoneWidget(),
         ],
+      ),
+    );
+  }
+}
+
+class ActivityDurationWidget extends StatelessWidget {
+  const ActivityDurationWidget({
+    super.key,
+    this.duration,
+  });
+
+  final Duration? duration;
+
+  @override
+  Widget build(BuildContext context) {
+    int? hours;
+    int? minutes;
+    int? seconds;
+    if (duration != null) {
+      hours = duration!.inHours;
+      minutes = duration!.inMinutes - hours * Duration.minutesPerHour;
+      seconds = duration!.inSeconds -
+          hours * Duration.minutesPerHour -
+          minutes * Duration.secondsPerMinute;
+    }
+
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Text(
+        style: DefaultTextStyle.of(context).style.apply(
+            fontSizeFactor: 3.0,
+            fontWeightDelta: 5,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+        '${hours != null ? NumberFormat('00').format(hours) : '--'}:'
+        '${minutes != null ? NumberFormat('00').format(minutes) : '--'}:'
+        '${seconds != null ? NumberFormat('00').format(seconds) : '--'}',
       ),
     );
   }
