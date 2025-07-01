41a3c616222b9cf2deae0194190ba9d02523abb9
bcroix <brice.croix@gmail.com>
2025-07-01 19:25:47 +0200

    Fixed drifting compass rotation

--- lib/presentation/screens/common/widgets/continuously_animated_rotation.dart
+++ lib/presentation/screens/common/widgets/continuously_animated_rotation.dart
@@ -25,32 +25,55 @@ class ContinuouslyAnimatedRotation extends ImplicitlyAnimatedWidget {
 class _ContinuouslyAnimatedRotationState
     extends AnimatedWidgetBaseState<ContinuouslyAnimatedRotation> {
   Tween<double>? _turnsTween;
-  double _currentTurns = 0.0;
+
+  /// Unbounded: used to compute deltas
+  double _rawTurns = 0.0;
+
+  /// Normalized value shown to the user when not animating.
+  double _displayTurns = 0.0;
 
   @override
   void forEachTween(TweenVisitor<dynamic> visitor) {
-    final newTurns = _normalizeAndComputeTargetTurns(widget.turns);
+    final targetTurns = widget.turns % 1.0;
+    final delta = targetTurns - (_rawTurns % 1.0);
+    final shortestDelta = delta - delta.roundToDouble();
+    final nextTurns = _rawTurns + shortestDelta;
+
     _turnsTween = visitor(
       _turnsTween,
-      newTurns,
+      nextTurns,
       (dynamic value) => Tween<double>(begin: value as double),
     ) as Tween<double>;
+
+    _rawTurns = nextTurns;
   }
 
-  /// Normalize delta into shortest arc [-0.5, 0.5] and accumulate it
-  double _normalizeAndComputeTargetTurns(double newTurns) {
-    final delta = newTurns - _currentTurns;
-    final shortestDelta = delta - delta.roundToDouble();
-    final targetTurns = _currentTurns + shortestDelta;
-    _currentTurns = targetTurns;
-    return targetTurns;
+  @override
+  void didUpdateTweens() {
+    super.didUpdateTweens();
+
+    // When animation completes, snap _displayTurns to the normalized equivalent
+    // in [0, 1)
+    animation.addStatusListener(_onAnimationStatusChanged);
+  }
+
+  void _onAnimationStatusChanged(AnimationStatus status) {
+    if (status == AnimationStatus.completed ||
+        status == AnimationStatus.dismissed) {
+      animation.removeStatusListener(_onAnimationStatusChanged);
+
+      // Normalize only the displayed value, raw value keeps continuity
+      _displayTurns = _rawTurns % 1.0;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final animatedTurns = _turnsTween?.evaluate(animation) ?? _currentTurns;
+    final turns = !animation.isCompleted && !animation.isDismissed
+        ? (_turnsTween?.evaluate(animation) ?? _displayTurns)
+        : _displayTurns;
     return RotationTransition(
-      turns: AlwaysStoppedAnimation(animatedTurns),
+      turns: AlwaysStoppedAnimation(turns),
       child: widget.child,
     );
   }
