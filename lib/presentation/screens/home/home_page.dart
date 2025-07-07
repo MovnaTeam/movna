fab2105a9036116a5485a796f54fa22e258de422
bcroix <brice.croix@gmail.com>
2025-07-08 00:45:27 +0200

    Add slide gesture detection

--- lib/presentation/screens/home/home_page.dart
+++ lib/presentation/screens/home/home_page.dart
@@ -29,15 +29,27 @@ class _HomePageState extends State<HomePage> {
           child: SvgThemedWidget(svgAsset: Assets.images.movnaLogo),
         ),
       ),
-      body: ValueListenableBuilder<int>(
-        valueListenable: _selectedTab,
-        builder: (context, selectedTab, _) {
-          return SlideIndexedStack(
-            index: selectedTab,
-            duration: Duration(milliseconds: 200),
-            children: tabs,
-          );
+      body: GestureDetector(
+        onHorizontalDragEnd: (details) {
+          const threshold = 300;
+          if (details.primaryVelocity == null) return;
+          if (details.primaryVelocity! > threshold && _selectedTab.value > 0) {
+            _selectedTab.value -= 1;
+          } else if (details.primaryVelocity! < -threshold &&
+              _selectedTab.value < tabs.length - 1) {
+            _selectedTab.value += 1;
+          }
         },
+        child: ValueListenableBuilder(
+          valueListenable: _selectedTab,
+          builder: (context, selectedTab, _) {
+            return SlideIndexedStack(
+              index: selectedTab,
+              duration: Duration(milliseconds: 200),
+              children: tabs,
+            );
+          },
+        ),
       ),
       bottomNavigationBar: ValueListenableBuilder(
         valueListenable: _selectedTab,
